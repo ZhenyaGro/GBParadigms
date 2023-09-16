8f99fa10eb809af0e5c20de5a4c6a6941127c2dd
ZhenyaGro <bon4ios@gmail.com>
2023-09-16 16:51:21 +0300

    Improve for terminal playing

--- seminar3/tictactoe.js
+++ seminar3/tictactoe.js
@@ -1,3 +1,5 @@
+const readline = require('readline');
+
 class TicTacToe {
   constructor() {
     this.board = [
@@ -6,34 +8,56 @@ class TicTacToe {
       ['', '', '']
     ];
     this.currentPlayer = 'X';
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  }
+
+  startGame() {
+    console.log('Игра началась. Для совершения хода введите номер строки (0-2) и номер столбца (0-2), разделенные пробелом.');
+
+    this.printBoard();
+    this.rl.on('line', (input) => {
+      const [row, col] = input.trim().split(' ');
+      this.makeMove(parseInt(row), parseInt(col));
+    });
   }
 
   makeMove(row, column) {
     if (this.isValidMove(row, column)) {
       this.board[row][column] = this.currentPlayer;
-      this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+      this.printBoard();
+
+      if (this.isGameFinished()) {
+        console.log(`Игра окончена.Победил игрок ${this.currentPlayer}!`);
+        this.rl.close();
+        process.exit();
+      } else {
+        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+      }
+    } else {
+      console.log('Недопустимый ход. Попробуйте еще раз.');
     }
   }
 
   isValidMove(row, column) {
-    // Проверяем, что выбранная клетка не занята другим игроком и находится в пределах доски
-    return this.board[row][column] === '' &&
+    return (
+      this.board[row][column] === '' &&
       row >= 0 && row < 3 &&
-      column >= 0 && column < 3;
+      column >= 0 && column < 3
+    );
   }
 
   isGameFinished() {
     // Проверяем выигрышные комбинации
     const winningCombinations = [
-      // горизонтальные комбинации
       [[0, 0], [0, 1], [0, 2]],
       [[1, 0], [1, 1], [1, 2]],
       [[2, 0], [2, 1], [2, 2]],
-      // вертикальные комбинации
       [[0, 0], [1, 0], [2, 0]],
       [[0, 1], [1, 1], [2, 1]],
       [[0, 2], [1, 2], [2, 2]],
-      // диагональные комбинации
       [[0, 0], [1, 1], [2, 2]],
       [[0, 2], [1, 1], [2, 0]]
     ];
@@ -55,34 +79,23 @@ class TicTacToe {
 
     // Проверяем ничью
     if (!this.board.flat().includes('')) {
-      return true;
+      console.log('Игра окончена. Ничья!');
+      this.rl.close();
+      process.exit();
     }
 
     return false;
   }
 
   printBoard() {
+    console.log('-------------');
     for (const row of this.board) {
-      console.log(row.join(' | '));
+      console.log(`| ${row.join(' | ')} |`);
+      console.log('-------------');
     }
   }
 }
 
-// Пример использования:
-
+// Запуск игры
 const game = new TicTacToe();
-game.makeMove(0, 0);
-game.makeMove(1, 1);
-game.makeMove(0, 1);
-game.makeMove(2, 2);
-game.makeMove(0, 2);
-
-game.printBoard();
-console.log(game.isGameFinished() ? 'Игра окончена' : 'Игра продолжается');
-
-// Вывод в консоль:
-// X | X | O
-//   | O |  
-//   |   | X
-// Игра окончена
-
+game.startGame();
